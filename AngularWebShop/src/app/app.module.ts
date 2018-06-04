dcbd81fdc542f6ca4ae93d8af92cece379e7308f
Gonzalo Concepción Megía <gonzaloconcepcionmegia@ua.pt>
2018-06-04 09:32:28 +0100

    Allow control fixed

--- AngularWebShop/src/app/app.module.ts
+++ AngularWebShop/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
